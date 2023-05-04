fa86d230617e6a6efe71688d498e6cc402aefbad
Yoav Klein <yoavklein25@gmail.com>
2023-05-04 14:30:46 +0000

    install tools

---
+++ install-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install Docker
+#curl -L get.docker.com | sudo bash
+
+# install kubectl
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#rm kubectl
+
+# install aws CLI
+sudo apt-get update
+sudo apt-get install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
